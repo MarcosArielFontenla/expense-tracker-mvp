273c38dda473a03e1f3a4284c148d87e92577564
Marcos Fontenla <marcos.fontenla@hotmail.com>
2025-11-23 14:15:54 -0300

    (feat): Added routes & navigation

---
+++ src/app/core/layout/layout.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Sidebar } from './sidebar/sidebar';
+
+@Component({
+    selector: 'app-layout',
+    imports: [CommonModule, RouterModule, Sidebar],
+    templateUrl: './layout.html',
+    styleUrl: './layout.css',
+    standalone: true
+})
+export class Layout { }
