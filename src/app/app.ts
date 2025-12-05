1a3b9d31fc4a43d9e54dd26a4957a29e6b90b110
Marcos Fontenla <marcos.fontenla@hotmail.com>
2025-12-05 16:28:56 -0300

    (feat): Added onboarding tutorial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnboardingOverlay } from './shared/components/onboarding-overlay/onboarding-overlay';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OnboardingOverlay],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
